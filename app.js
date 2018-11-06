d550cd93c884b3516a1f7ec5bec03205fe95e258
Scott M. Oloier <00SMO@dd.local>
2018-11-06 14:53:06 -0500

    Fixed incorrect image URL in buildObjects. Cleaned up environment, removed original project libs.

--- app.js
+++ app.js
@@ -1,57 +1,17 @@
 require('dotenv').config()
 const path = require('path')
 const fetchGet = require('./lib/fetchGet')
+const rssPrep = require('./lib/feedItem')
 const express = require('express')
 const app = express()
 app.disable('x-powered-by') // remove the 'X-Powered-By: Express' header
-app.set('json spaces', 2) // basic pretty-print
 
-// Register Handlebars view engine
+// register Handlebars view engine
 app.set('view engine', 'handlebars')
 const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs())
 app.set('view engine', 'handlebars')
-
-// default to pug for rendering
-// app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, '/views'))
-
-
-const rssPrep = require('./lib/feeditem')
-
-// app.get('/r/:subreddit/time/:time/:limit', async (req, res) => {
-// 	try {
-// 		let subreddit = req.params.subreddit
-// 		let time = req.params.time
-// 		let limit = req.params.limit
-
-// 		const feed = `https://www.reddit.com/r/${subreddit}/top/.json?sort=top&t=${time}&limit=${limit}`
-
-// 		let json = await fetchGet(feed)
-// 		let jsonArray = json.data.children 
-
-// 		let rssObjects = []
-// 		jsonArray.forEach(async (entry) => {
-// 			let rss = new RssItem(entry)
-// 			await rss.init2()
-// 			console.log(rss)
-// 			rssObjects.push(rss)
-// 		})
-
-// 		res.set('Content-Type', 'application/rss+xml')
-// 		res.render('rss', {
-// 			doctype:'xml',
-// 			data: {
-// 				subreddit: jsonArray[0].data.subreddit,
-// 				link: jsonArray[0].data.subreddit,
-// 				items:  await rssObjects
-// 			}
-// 		})
-// 	} catch (ex) {
-// 		console.error(ex)
-// 	}
-
-// })
 
 app.get('/r/:subreddit/time/:time/:limit', async (req, res) => {
 	try {
