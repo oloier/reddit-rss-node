640df69ff4aa7873a04901cbdeda5d48d54f2d83
Scott M. Oloier <00SMO@dd.local>
2018-11-13 11:47:10 -0500

    Resolved imgur extensionless URLs not embedding. Fixed redundant thumbnail attachments on image-only content. Fixed incorrect array comparison for img exts. Fixed selftext pointers and rendering.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const fs = require('fs')
-const fastify = require('fastify')({logger: false})
+const fastify = require('fastify')({
+	logger: false
+})
 const _ = require('lodash')
 const fetch = require('node-fetch')
 
@@ -17,7 +19,9 @@ fastify.get('/v/:url', (request, resp) => {
 	resp.header('x-content-type-options', 'nosniff')
 	resp.header('strict-transport-security', 'max-age=31536000')
 	// invoke lodash template string, send as response
-	resp.send(render('video-iframe.html', {url}))
+	resp.send(render('video-iframe.html', {
+		url
+	}))
 })
 
 fastify.get('/:subreddit/top-:time/limit-:limit', async (request, resp) => {
@@ -25,7 +29,7 @@ fastify.get('/:subreddit/top-:time/limit-:limit', async (request, resp) => {
 		const subreddit = request.params.subreddit || 'all'
 		const time = request.params.time || 'day'
 		const limit = request.params.limit || 5
-		
+
 		const feed = `https://www.reddit.com/r/${subreddit}/top/.json?sort=top&t=${time}&limit=${limit}`
 		// console.info(`URL request: ${feed}`)
 
@@ -38,12 +42,11 @@ fastify.get('/:subreddit/top-:time/limit-:limit', async (request, resp) => {
 		}
 
 		// invoke lodash template string, send as responsenpm s
-		resp.header('content-type', 'application/rss+xml')
+		resp.header('content-type', 'application/rss+xml; charset=utf-8')
 		resp.send(render('rss.xml', rssMomma))
 	} catch (ex) {
 		console.error(ex)
 	}
-
 })
 
 const fetchGet = async (url) => {
@@ -70,7 +73,7 @@ const prepareFeedItems = (rdtPost) => {
 			nsfw: (!!rdt.data.over_18),
 			secure_embed: rdt.data.secure_media_embed || '',
 			selftext: rdt.data.selftext_html || '',
-			post_hint: rdt.data.post_hint,
+			post_hint: rdt.data.post_hint || 'self',
 			num_comments: rdt.data.num_comments,
 			thumbnail: {
 				url: (rdt.data.thumbnail !== 'self') ? rdt.data.thumbnail : null,
@@ -79,8 +82,9 @@ const prepareFeedItems = (rdtPost) => {
 			}
 		}
 
+
 		// small little self-contained iframe video embedding
-		const videoTemplate = (url, width=640, height=420) => {
+		const videoTemplate = (url, width = 640, height = 420) => {
 			return `<iframe width=${width} height=${height} frameborder=0 src="https://oloier.com/r/v/${encodeURIComponent(url)}"></iframe>`
 		}
 
@@ -94,37 +98,57 @@ const prepareFeedItems = (rdtPost) => {
 			item.content = videoTemplate(rv.fallback_url, rv.width, rv.height)
 		}
 
-		// direct image embedding
+		// embeddable image extensions
 		let exts = ['.jpg', '.png', '.webp', '.gif', '.jpeg']
+		
+		// direct image embedding
 		if (item.post_hint && item.post_hint.indexOf('image') !== -1 ||
-			exts.indexOf(item.url) !== -1) {
+			item.url.containsAny(exts)) {
 			item.content = `<img src="${item.url}" alt="">`
+			item.thumbnail.url = null
 		}
 
 		// do your best, self text... who goddamn cares. No post_hint for self. Stupid?
 		if (item.selftext) {
+			item.selftext = item.selftext.replace('<!-- SC_OFF -->', '').replace('<!-- SC_ON -->', '')
 			item.content = _.unescape(item.selftext)
-			item.thumbnail = null
+			item.thumbnail.url = null
 		}
-
+		
 		// individual other site exceptions, imgur, instagram, etc.
 		if (item.post_hint && item.post_hint.indexOf('link') !== -1) {
-			if (item.domain.indexOf('imgur.com') !== -1 && item.url.indexOf('.gifv' !== -1)) {
-				// replace gifv with <video> embed of mp4 source URL
-				//	 item.content = videoTemplate(item.url.replace('.gifv', '.mp4'))
-				let rvp = rdt.data.preview.reddit_video_preview
-				item.content = videoTemplate(rvp.fallback_url, rvp.width, rvp.height)
+			// old way: item.url.replace('gifv', 'mp4')
+			if (item.domain.indexOf('imgur.com') !== -1) {
+				
+				if (item.url.indexOf('.gifv') !== -1) {
+					let rvp = rdt.data.preview.reddit_video_preview
+					item.content = videoTemplate(rvp.fallback_url, rvp.width, rvp.height)
+				}
+
+				// if imgur link is not a gifv and not a direct link,
+				// just force an image extension because it's faster in 
+				// this case than calling out to their API. It's also 
+				// VERY lazy, but we actually don't need to go that far.
+				if (!item.url.containsAny(exts)) {
+					item.content = `<img src="${item.url + exts[0]}" alt="">`
+					item.thumbnail.url = null
+				}
 			}
 		}
-
 		// hide NSFW content, or any content for a straight link
 		if (item.nsfw) item.content = ''
 
-		console.log(item)
 		items.push(item)
 	})
 	return items
 }
+
+// if string contains array substr
+String.prototype.containsAny = String.prototype.containsAny || function(arr) {
+	for (var i = 0; i < arr.length; i++) if (this.indexOf(arr[i]) > -1) return true
+	return false
+}
+
 
 // lodash template compilation
 function render(view, ctx = {}) {
