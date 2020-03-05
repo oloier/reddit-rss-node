677b6672e90ad41e05279674eaec0cb49acd495f
Scott Oloier <scottoloier@gmail.com>
2020-03-05 13:59:40 -0500

    containsAny requires array argument

--- app.js
+++ app.js
@@ -116,7 +116,7 @@ const prepareFeedItems = (rdtPost) => {
 		// rich:video is anything with oembed
 		if (item.post_hint == "rich:video") {
 			item.content = _.unescape(item.secure_embed.content)
-			if (item.content.containsAny("youtube.com/embed/")) {
+			if (item.content.containsAny(["youtube.com/embed/"])) {
 				item.content.replace("&enablejsapi=1", "&enablejsapi=1&modestbranding=1&ytp-pause-overlay=0")
 			}
 		}
